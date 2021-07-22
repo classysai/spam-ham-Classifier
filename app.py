4169600147a0857a183491cc564c0613d46f5677
Sai Kiran <84656277+classysai@users.noreply.github.com>
2021-07-22 09:43:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import joblib
-model=joblib.load("Email_model")
+model=joblib.load("spam-ham")
 st.title('Spam Ham Classifier')
+st.image("/content/OIP.jpeg",width=800,)
 ip=st.text_input("Enter the message")
 op=model.predict([ip])
 if st.button("predict"):
   st.title(op[0])
-    
+  st.subheader("successfully Done by the sai kian")
